6d56fece1a55e9044ac65e42952d949114ab93cb
Clem005 <154925422+Clem005@users.noreply.github.com>
2026-05-10 23:35:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,15 @@
 // 1. THE MANIFEST
 // Add every filename you create in the /posts/ folder here.
 const POST_FILES = [
-    "post1.json",
-    "introduction-and-strategic-framing-of-is.json"
+    "week1.json",
+    "week2.json",
+    "week3.json",
+    "week4.json",
+    "week5.json",
+    "week6.json",
+    "week7.json",
+    "week8.json"
+    
 ];
 
 let globalPosts = [];
